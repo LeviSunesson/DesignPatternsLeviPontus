e869e20e9554bf0447ed9ec8d82fda326a081315
PontusFransson <pontus.fransson.te16d@gmail.com>
2019-01-29 15:45:49 +0100

    Update Volvo740.java

--- src/task_A/Volvo740.java
+++ src/task_A/Volvo740.java
@@ -4,7 +4,7 @@ import javafx.scene.paint.Color;
 
 public class Volvo740 extends Car {
 
-	public final static double trimFactor = 1.25;
+	public final static double trimFactor = 1.25; //
 
 	public Volvo740() {
 		
@@ -12,6 +12,10 @@ public class Volvo740 extends Car {
 		
 	}
 
+	/**
+	 * speedFactor is calculating the speedFactor depending on trimFactor
+	 * @return The enginePower as a decimal number
+	 */
 	@Override
 	public double speedFactor() {
 		return getEnginePower() * 0.01 * trimFactor;
