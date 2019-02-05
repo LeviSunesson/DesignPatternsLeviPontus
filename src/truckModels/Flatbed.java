240dfb3e90894454ddb64c974c2c5f5feda90a73
leviSunesson <levi@sunesson.com>
2019-02-05 15:53:36 +0100

    Update Flatbed.java

--- src/truckModels/Flatbed.java
+++ src/truckModels/Flatbed.java
@@ -2,25 +2,59 @@ package truckModels;
 
 public class Flatbed {
 
+	//private boolean isDown = true;
+	private double angle = 0;
+	//If the angle is 0 then the flatbed is down, else it is up
+	
 	public Flatbed() {
 		
 		
 		
 	}
 	
-	public void higher() {
+	public boolean isDown() {
+		
+		return (angle == 0) ? true : false;
 		
 	}
 	
-	public void lower() {
+	public void higher(int amount) {
+		
+		if(amount > 90-angle) {
+			
+			amount = (int) (90-angle);
+			
+		}else if(amount < 0) {
+			
+			amount = 0;
+			
+		}else if(angle == 90) {
+			
+			return;
+			
+		}
+		
+		angle += amount;
 		
 	}
 	
-	public void load() {
+	public void lower(int amount) {
 		
-	}
-	
-	public void unload() {
+		if(amount > angle) {
+			
+			amount = (int) (angle);
+			
+		}else if(angle == 0) {
+			
+			return;
+			
+		}else if(amount < 0) {
+			
+			amount = 0;
+			
+		}
+		
+		angle -= amount;
 		
 	}
 	
