6d474541a25f690c5ac888fff1cafaf4a9781350
leviSunesson <levi@sunesson.com>
2019-02-26 15:47:45 +0100

    Started volvo testing

---
+++ src/tests/Volvo740_Test.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.junit.Test;
+
+import abstracts.Car;
+import carModels.Volvo740;
+
+public class Volvo740_Test {
+
+	Car volvo = new Volvo740();
+
+	@Test
+	public void startCarTest() {
+		
+		
+		
+		
+	}
+	
+}
