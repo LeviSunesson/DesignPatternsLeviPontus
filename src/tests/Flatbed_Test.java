8e640016207f51851ea52ad1b3eb52018fd3a027
PontusFransson <pontus.fransson.te16d@gmail.com>
2019-03-01 12:45:13 +0100

    Flatbed_Test done

--- src/tests/Flatbed_Test.java
+++ src/tests/Flatbed_Test.java
@@ -1,16 +1,42 @@
 package tests;
 
+
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
+import truckModels.Flatbed;
+import truckModels.Scania;
 
 public class Flatbed_Test {
 
-	
+	Scania scania = new Scania();
+	Flatbed flatbed = new Flatbed();
 	
 	@Test
 	public void startTruckTest() {
 		
+		scania.raise(10);
+		scania.startEngine();
+		assertEquals(false,scania.isMoving());
+		scania.gas(1);
+		scania.move();
+		assertEquals(0, scania.getCurrentSpeed(),0);
+		assertEquals(0, scania.getPos()[0],0);
+		assertEquals(0, scania.getPos()[1],0);
 		
-
+		scania.lower(10);
+		scania.startEngine();
+		scania.gas(1);
+		scania.move();
+		assertEquals(0.41, scania.getCurrentSpeed(),0.01);
+		assertEquals(0.41, scania.getPos()[0],0.01);
+		assertEquals(0, scania.getPos()[1],0.01);
+		
+		flatbed.open();
+		assertEquals(90, flatbed.getAngle(), 0);
+		flatbed.close();
+		assertEquals(0, flatbed.getAngle(), 0);
 		
 	}
 	
